6465bd29c3c97d825aff9ef791b37c513aa31f5a
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-08 13:27:20 -0300

    feat: create next app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex bg-violet-900 min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl text-white font-bold">Next Boilerplate</h1>
+    </main>
+  );
+}
