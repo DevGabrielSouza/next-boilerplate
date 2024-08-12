d689a4495d6d0841d9df9d11116de9a8d95d76ae
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-12 12:53:45 -0300

    chore: remove unused code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,4 @@
-import { useTranslations } from 'next-intl'
-import Image from 'next/image'
-
 export default function Home() {
-  const t = useTranslations()
   return (
     <main className='flex bg-violet-900 min-h-screen flex-col items-center justify-center'>
       <h1 className='text-4xl text-white font-bold'>Next Boilerplate</h1>
