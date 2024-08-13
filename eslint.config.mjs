4da7a001382679f9147dcc8f6b54a4c96cce9ccf
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-13 16:12:17 -0300

    chore: update eslint config and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,67 +1,12 @@
-// Import the necessary plugins and parsers
-import eslintPluginReact from 'eslint-plugin-react'
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
-import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin'
-import eslintParserTypeScript from '@typescript-eslint/parser'
-import eslintPluginImport from 'eslint-plugin-import'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 
 export default [
-  {
-    files: ['*.ts', '*.tsx'],
-    languageOptions: {
-      parser: eslintParserTypeScript,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true
-        },
-        project: './tsconfig.json'
-      }
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
-    env: {
-      browser: true,
-      es2020: true,
-      node: true
-    },
-    plugins: {
-      react: eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
-      '@typescript-eslint': eslintPluginTypeScript,
-      import: eslintPluginImport,
-      prettier: eslintPluginPrettier
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:@next/next/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'prettier',
-      'plugin:prettier/recommended'
-    ],
-    rules: {
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_' }
-      ],
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-      'import/no-unused-modules': [1, { unusedExports: true }]
-    },
-    ignorePatterns: ['node_modules', 'build', 'dist']
-  }
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended
 ]
