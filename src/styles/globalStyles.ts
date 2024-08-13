ab8fbe994b2f3ed231be2c72c4c01dbd830d6cc1
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-13 15:33:04 -0300

    chore: remove global background prop

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -10,6 +10,5 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: ${(props) => props.theme.fonts.main};
-    background-color: ${(props) => props.theme.colors.background};
   }
 `
