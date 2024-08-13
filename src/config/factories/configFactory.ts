72158dd0e6a7f231b4fc67c22c09bf2ba09e23fa
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-13 15:34:13 -0300

    fix: add missing slash to base api url

--- src/config/factories/configFactory.ts
+++ src/config/factories/configFactory.ts
@@ -13,7 +13,7 @@ export const configFactory = (overrides: Partial<Config>): Config => {
     isDevelopment: false,
     isTest: false,
     port: parseInt(process.env.PORT || '3000', 10),
-    apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001/',
+    apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001',
     frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000'
   }
 
